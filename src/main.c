b0b9164c035e08a02e016d6401a077497a7f4f57
Brochu <alexbrochu1@gmail.com>
2024-03-10 04:07:41 -0400

    [DEV] Testing dynamic array with char*

--- src/main.c
+++ src/main.c
@@ -1,26 +1,24 @@
 #include "darray.h"
-
-#include <stdint.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
     printf("[MAIN] Trying my hand at C??\n");
 
     printf("[MAIN] Creating a dynamic array ...\n");
-    uint64_t *myArray = arr_init(uint64_t);
+    char **myArray = arr_init(char*);
 
     size_t size = arr_size(myArray);
     size_t cap = arr_cap(myArray);
     size_t it_size = arr_itemsize(myArray);
     printf("[MAIN] myArray { size: %lld; cap: %lld; item: %lld }\n", size, cap, it_size);
-    for (int i = 0; i < 10; i++) {
-        arr_push(myArray, i);
+    for (int i = 0; i < 300; i++) {
+        arr_push(myArray, "Testing...");
     }
     size = arr_size(myArray);
     cap = arr_cap(myArray);
     printf("[MAIN] myArray { size: %lld; cap: %lld }\n", size, cap);
     for (int i = 0; i < arr_size(myArray); i++) {
-        printf(" - %lld \n", myArray[i]);
+        printf("%s, ", myArray[i]);
     }
 
     arr_free(myArray);
