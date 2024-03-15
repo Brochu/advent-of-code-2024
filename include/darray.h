1b442b7ea11cae523a3f59a3df7618665dfe8bad
Brochu <alexbrochu1@gmail.com>
2024-03-15 12:48:37 -0400

    [DEV] Adapted multi statement macro with do.. while(0)

--- include/darray.h
+++ include/darray.h
@@ -7,13 +7,17 @@ typedef struct {
     size_t pad;
 } header;
 
-#define _ARR_INIT_CAP 256
+#define _ARR_INIT_CAP 8
 #define arr_init(T) (_arr_init(sizeof(T), _ARR_INIT_CAP))
 #define arr_header(a) ((header *)a - 1)
 #define arr_size(a) (arr_header(a)->size)
 #define arr_cap(a) (arr_header(a)->cap)
 #define arr_itemsize(a) (arr_header(a)->item_size)
-#define arr_push(a, v) _arr_adapt((void**)&a, 1); a[arr_size(a)]=v; arr_header(a)->size++
+#define arr_push(a, v) do {     \
+    _arr_adapt((void**)&a, 1);  \
+    a[arr_size(a)] = v;         \
+    arr_header(a)->size++;       \
+} while(0)
 #define arr_popback(a) _arr_popback(a)
 #define arr_erase(a, i) _arr_erase(a, i, &a[i + 1], &a[i])
 #define arr_free(a) (_arr_free(a))
