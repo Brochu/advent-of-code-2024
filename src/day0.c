b532c76c5d4939559d2b0e7d5b263e39c13ffd6e
Brochu <alexbrochu1@gmail.com>
2024-03-22 14:30:16 -0400

    [DEV] Trying with different puzzle

--- src/day0.c
+++ src/day0.c
@@ -4,90 +4,43 @@
 #include "strview.h"
 
 #include <stdio.h>
-#include <stdlib.h>
 
 #define DEMO 1
 #if DEMO == 1 // ------------------------------------
-//#define FILE_PATH ".\\inputs\\day0_demo1.txt"
-#define FILE_PATH ".\\inputs\\file.txt"
+#define FILE_PATH ".\\inputs\\day0_demo.txt"
 #else // ------------------------------------
 #define FILE_PATH ".\\inputs\\day0.txt"
 #endif // ------------------------------------
 
-typedef struct game {
-    size_t id;
-    int maxes[3];
-} game;
+size_t stride = 0;
+int *grid = 0;
 
-int part1(char *out, game *games) {
-    size_t sum = 0;
-    int totals[3] = { 12, 13, 14 };
-
-    //printf("Games: size=%lld, cap=%lld\n", arr_size(games), arr_cap(games));
-    for (int i = 0; i < arr_size(games); i++) {
-        //printf("Game(%lld) -> [r]: %i ; [g]: %i ; [b]: %i\n",
-        //       games[i].id, games[i].maxes[0], games[i].maxes[1], games[i].maxes[2]);
-        if (games[i].maxes[0] <= totals[0] && games[i].maxes[1] <= totals[1] && games[i].maxes[2] <= totals[2]) {
-            sum += games[i].id;
-        }
-    }
-
-    sprintf_s(out, RES_MAX_LENGTH, "%zd", sum);
+int part1(char *out) {
+    sprintf_s(out, RES_MAX_LENGTH, "%d", 0);
     return 0;
 }
 
-int part2(char *out, game *games) {
-    size_t sum = 0;
-
-    for (int i = 0; i < arr_size(games); i++) {
-        size_t sub = 1;
-        sub *= games[i].maxes[0];
-        sub *= games[i].maxes[1];
-        sub *= games[i].maxes[2];
-        //printf("[Game %zd] -> subsum = %zd\n", games[i].id, sub);
-
-        sum += sub;
-    }
-
-    sprintf_s(out, RES_MAX_LENGTH, "%zd", sum);
+int part2(char *out) {
+    sprintf_s(out, RES_MAX_LENGTH, "%d", 0);
     return 0;
 }
 
 
 int run(char *part1_out, char *part2_out) {
     char *in = read_file(FILE_PATH);
-    game *games = arr_init(game);
 
     strview *lines = chr_split(in, "\n");
     for (int i = 0; i < arr_size(lines); i++) {
-        strview first;
-        strview rest;
-        str_split_once(lines[i], ": ", &first, &rest);
-
-        size_t id = 0;
-        sscanf_s(first.ptr, "Game %lld", &id);
-        game g = { .id = id, .maxes = {0} };
-
-        strview *draws = str_split(rest, "; ");
-        for (int j = 0; j < arr_size(draws); j++) {
-            strview *elems = str_split(draws[j], ", ");
-            for (int k = 0; k < arr_size(elems); k++) {
-                strview count;
-                strview color;
-                str_split_once(elems[k], " ", &count, &color);
-
-                int c = 0;
-                sscanf_s(count.ptr, "%i", &c);
-                char col = color.ptr[0];
-                g.maxes[col%3] = max(g.maxes[col%3], c);
-            }
+        strview *nodes = str_split(lines[i], ", ");
+        for (int j = 0; j < arr_size(nodes); j++) {
+            printf("'%.*s'", (int)nodes[j].size, nodes[j].ptr);
         }
-        arr_push(games, g);
+        printf("\n");
     }
 
-    part1(part1_out, games);
-    part2(part2_out, games);
+    part1(part1_out);
+    part2(part2_out);
 
-    free(in);
+    //free(in);
     return 0;
 }
