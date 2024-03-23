8b3d6cbd924d244fe78f10374289300096f566fd
Brochu <alexbrochu1@gmail.com>
2024-03-22 21:27:00 -0400

    [DEV] Build 2d grid based off input

--- src/day0.c
+++ src/day0.c
@@ -4,6 +4,7 @@
 #include "strview.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DEMO 1
 #if DEMO == 1 // ------------------------------------
@@ -30,17 +31,26 @@ int run(char *part1_out, char *part2_out) {
     char *in = read_file(FILE_PATH);
 
     strview *lines = chr_split(in, "\n");
+    stride = arr_size(lines);
+    grid = calloc(stride * stride, sizeof(int));
+
     for (int i = 0; i < arr_size(lines); i++) {
         strview *nodes = str_split(lines[i], ", ");
         for (int j = 0; j < arr_size(nodes); j++) {
             printf("'%.*s'", (int)nodes[j].size, nodes[j].ptr);
+            grid[j + (i * stride)] = atoi(nodes[j].ptr);
         }
         printf("\n");
+    }
+
+    for (int i = 0; i < stride * stride; i++) {
+        printf("arr[%i] = %i\n", i, grid[i]);
     }
 
     part1(part1_out);
     part2(part2_out);
 
-    //free(in);
+    free(grid);
+    free(in);
     return 0;
 }
