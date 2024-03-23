f0ca82310ad26c6450bd1e8cf5dc91f8fa9c5f6f
Brochu <alexbrochu1@gmail.com>
2024-03-22 21:35:26 -0400

    [DEV] Store puzzle input in a struct

--- src/day0.c
+++ src/day0.c
@@ -13,44 +13,49 @@
 #define FILE_PATH ".\\inputs\\day0.txt"
 #endif // ------------------------------------
 
-size_t stride = 0;
-int *grid = 0;
+typedef struct {
+    size_t stride;
+    int *vals;
+} grid;
 
-int part1(char *out) {
+void debug_grid(grid *g) {
+    for (int i = 0; i < g->stride * g->stride; i++) {
+        printf("[%i] -> %i\n", i, g->vals[i]);
+    }
+}
+
+int part1(grid *g, char *out) {
+    debug_grid(g);
     sprintf_s(out, RES_MAX_LENGTH, "%d", 0);
     return 0;
 }
 
-int part2(char *out) {
+int part2(grid *g, char *out) {
     sprintf_s(out, RES_MAX_LENGTH, "%d", 0);
     return 0;
 }
-
 
 int run(char *part1_out, char *part2_out) {
     char *in = read_file(FILE_PATH);
+    grid g = { .vals = NULL, .stride = 0 };
 
     strview *lines = chr_split(in, "\n");
-    stride = arr_size(lines);
-    grid = calloc(stride * stride, sizeof(int));
+    g.stride = arr_size(lines);
+    g.vals = calloc(g.stride * g.stride, sizeof(int));
 
     for (int i = 0; i < arr_size(lines); i++) {
         strview *nodes = str_split(lines[i], ", ");
         for (int j = 0; j < arr_size(nodes); j++) {
             printf("'%.*s'", (int)nodes[j].size, nodes[j].ptr);
-            grid[j + (i * stride)] = atoi(nodes[j].ptr);
+            g.vals[j + (i * g.stride)] = atoi(nodes[j].ptr);
         }
         printf("\n");
     }
 
-    for (int i = 0; i < stride * stride; i++) {
-        printf("arr[%i] = %i\n", i, grid[i]);
-    }
+    part1(&g, part1_out);
+    part2(&g, part2_out);
 
-    part1(part1_out);
-    part2(part2_out);
-
-    free(grid);
+    free(g.vals);
     free(in);
     return 0;
 }
