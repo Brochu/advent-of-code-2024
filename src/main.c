980540cf0c29edcaae2a281c35c240be47109b4c
Brochu <alexbrochu1@gmail.com>
2024-09-02 20:04:04 -0400

    [DEV] Rework handling of parts buffers for main file
    Still need to integrate a way to have raylib active for each days

--- src/main.c
+++ src/main.c
@@ -4,22 +4,44 @@
 
 #include "day.h"
 #include "darray.h"
-
 #include "raylib.h"
+
+int init_day(char **out_p1, char **out_p2) {
+    *out_p1 = calloc(RES_MAX_LENGTH, sizeof(char));
+    if (*out_p1 == NULL) {
+        return -1;
+    }
+
+    *out_p2 = calloc(RES_MAX_LENGTH, sizeof(char));
+    if (*out_p2 == NULL) {
+        return -1;
+    }
+
+    return 0;
+}
+
+void clean_day(char *p1, char *p2) {
+    free(p1);
+    p1 = NULL;
+    free(p2);
+    p2 = NULL;
+}
 
 int main(int argc, char **argv) {
     printf("[MAIN] Trying my hand at C??\n");
 
-    char *p1 = calloc(RES_MAX_LENGTH, sizeof(char));
-    char *p2 = calloc(RES_MAX_LENGTH, sizeof(char));
-    int code = run(p1, p2);
+    char *p1 = NULL;
+    char *p2 = NULL;
+    if (init_day(&p1, &p2) < 0) {
+        printf("[MAIN] Could not initialize answer buffers\n");
+        return EXIT_FAILURE;
+    }
 
+    int code = run(p1, p2);
     printf("[MAIN] Puzzle solution (code = %i)\n", code);
     printf(" - Part 1 = '%s'\n", p1);
     printf(" - Part 2 = '%s'\n", p2);
-
-    free(p1);
-    free(p2);
+    clean_day(p1, p2);
 
     if (argc > 1 && strcmp(argv[1], "--test") == 0) {
         _arr_tests();
@@ -35,5 +57,5 @@ int main(int argc, char **argv) {
         CloseWindow();
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
